2974c6af92cfb50ee6fae6878dcbfc17af557d1d
ray5wang <ray5wang@163.com>
2018-01-02 10:30:20 +0800

    读取二进制麦克风数据ok

--- demo.c
+++ demo.c
@@ -17,9 +17,9 @@ struct mic_Array{
 };
 
 /*varlue*/
-const char  binfileAddr[12]="./test.bin ";   //bin文件的路径
-struct mic_Array mic[8];
-struct mic_Array ss[8];
+const char  binfileAddr[2][12]={"./mic16.bin","tdoa1.bin"}; //bin文件的路径
+struct mic_Array mic[FrameSize];
+struct mic_Array ss[FrameSize];
 
 /* function  */
 void showfft(float *real ,float *imag);
@@ -27,12 +27,13 @@ void caculate_gccphat(struct mic_Array  *mic);
 void sprphat();
 void point_multi(struct mic_Array data1,struct mic_Array data2,float *result_real,float *result_Imag,int N);
 void init_func();
-int data_read();
+int data_read(int file);
 //int main(int argc, char* argv[])
 int main()
 {
     init_func();
-    caculate_gccphat(mic);
+    data_read(0);
+   // caculate_gccphat(mic);
 }
 
 
@@ -78,17 +79,16 @@ void caculate_gccphat(struct mic_Array *mic){
 //********************************
 /*cacullate the point multi of */
 //********************************
-void point_multi(struct mic_Array data1,struct mic_Array data2,float *result_real,float *result_Imag,int N){
-    int i;
-    float phat;
-    for(i=0;i<N;i++){
-    result_real[i] =  data1.mic_real[i] * data2.mic_real[i] - data1.mic_Imag[i] * data2.mic_Imag[i];
-    result_Imag[i] =  data1.mic_real[i] * data2.mic_Imag[i] + data1.mic_Imag[i] * data2.mic_real[i];
-   // phat = sqrt( result_real[i]* result_real[i]+result_Imag[i]*result_Imag[i] );
-    phat = 1;//debug 用
-   // result_real[i] = result_real[i]/phat;
-   // result_Imag[i] = result_Imag[i]/phat;
-    }
+void point_multi(struct mic_Array data1,struct mic_Array data2,float*result_real,float *result_Imag,int N){
+   int i;
+   double phat;
+   for(i=0;i<N;i++){
+   result_real[i] =  data1.mic_real[i] * data2.mic_real[i] - data1.mic_Imag[i] * data2.mic_Imag[i];
+   result_Imag[i] =  data1.mic_real[i] * data2.mic_Imag[i] + data1.mic_Imag[i] * data2.mic_real[i];
+   phat = sqrt( result_real[i]* result_real[i]+result_Imag[i]*result_Imag[i] );
+   result_real[i] = result_real[i]/phat;
+   result_Imag[i] = result_Imag[i]/phat;
+   }
 }
 
 //show the result of fft
@@ -114,20 +114,24 @@ void sprphat(){
  *micN   type:double      
  *tdoa                      */
 //***********************
-int  data_read(){
-  double num=0;
-  int i;
+int data_read(int file){
+  float num=0;
+  int i,j;
   FILE *fp;//判断命令行是否正确
             //按读方式打开由argv[1]指出的文件
-  if((fp=fopen(binfileAddr,"r"))==NULL){
-        printf("The file <%s> can not be opened.\n",binfileAddr);//打开操作不成功
-            return -1;//结束程序的执行
-                }
-                    for(i=1;i<=256;i++){
-       // fscanf(fp, "%f",num);
-           fread( &num, sizeof(double), 1, fp ); 
-               printf("The num %d is %lf\n",i,num);
+  if((fp=fopen(binfileAddr[file],"r"))==NULL){
+    printf("The file <%s> can not be opened.\n",binfileAddr[file]);//打开操作不成功
+    return -1;//结束程序的执行
+  }
+  for(i=0;i<MIC;i++){
+    for(j=0;j<FrameSize;j++){
+        fread( &num, sizeof(float), 1, fp ); 
+        mic[1].mic_signal[j]=num;    
+        printf("%f ",num);
     }
-    fclose(fp); //关闭fp所指文件
-    return 0;
+    printf("\n");
+   // printf("The num %d is %lf\n",i,num);
+  }
+  fclose(fp); //关闭fp所指文件
+  return 0;
  }
